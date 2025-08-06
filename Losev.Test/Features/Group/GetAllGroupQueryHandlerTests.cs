ab1c50ef57aca3444101954e18925ff2ec93c4c2
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-08-06 14:58:54 +0300

    ek dosylarar

---
+++ Losev.Test/Features/Group/GetAllGroupQueryHandlerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Losev.Test.Features.Group
+{
+    internal class GetAllGroupQueryHandlerTests
+    {
+    }
+}
